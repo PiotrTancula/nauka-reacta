35b09739c47aa55a4f273ecbff64f26f57a78334
Piotr Tańcula <tanculapiotr@gmail.com>
2022-07-13 18:28:24 +0200

    init commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import styles from './Button.module.scss';
+
+const Button = (props) => {
+    return <button className={styles.button} text={props.text} type="text">{props.children}</button>
+
+};
+
+export default Button;
