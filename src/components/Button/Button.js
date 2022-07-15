2465621b3a6ec101005820bcd3df2cb1688c61c6
Piotr Tańcula <tanculapiotr@gmail.com>
2022-07-15 17:15:10 +0200

    adding columns and rows inside is possible now

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,7 @@
 import styles from './Button.module.scss';
 
 const Button = (props) => {
-    return <button className={styles.button} text={props.text} type="text">{props.children}</button>
-
+  return <button className={styles.button} text={props.text} type="text">{props.children}</button>
 };
 
 export default Button;
